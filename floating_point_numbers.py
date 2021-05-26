98d0f1fad5cd9a4b93964f242ba58514954b99cc
Hussein-Mansour <hussein.mansour@ocsbstudent.ca>
2021-05-26 14:24:11 +0000

    added in floating_point_numbers.py

---
+++ floating_point_numbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Hussein Mansour
+# Created on: Tue/May25/2021
+# This program is a "Floating Point Numbers"
+
+
+def main():
+    # this function calculates the float numbers
+
+    # input
+    number_1 = float(input("Enter the number: "))
+    number_2 = float(input("Enter the number: "))
+    # process
+    add = number_1 + number_2
+    # output 
+    print("the sum = {0}".format(add))
+
+
+if __name__ == "__main__":
+    main()
